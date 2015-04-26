c98c6c1b37120f5807f765d897702facb30be815
Dwayne Thomas <thomas.dwayne@gmail.com>
2015-04-26 13:52:56 -0700

    got rid of javascript token error in chrome browser.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,17 @@
 
 function save() {
     var fieldValue = document.getElementById('nwnmid').value;
-    localStorage.setItem('text', fieldValue);
+    localStorage.setItem('name', fieldValue);
+    var fieldValue2 = document.getElementById('nwphnid').value;
+    localStorage.setItem('phone', fieldValue2);
 
-}
+    }
+
+function load() {
+    var storedValue = localStorage.getItem('name');
+    var storedValue2 = localStorage.getItem('phone');
+    if (storedValue || storedValue2) {
+        document.getElementById('nwnmid').value = storedValue;
+        document.getElementById('nwphnid').value = storedValue;
+                    }
+    }
